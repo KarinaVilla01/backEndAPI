9b2b55570e3772dcb9ef25df3bae5ebb789b86b9
KarinaVilla01 <104106557+KarinaVilla01@users.noreply.github.com>
2023-12-13 17:26:02 -0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,28 @@
 // src/index.js
 import express, { Express, Request, Response } from "express"
 import cors from "cors"
+import axios from "axios"
 
-const port = 3000
+const port = 3001
 
 const app: Express = express()
 app.use(cors())
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('This is my root route')
+app.get('/', async (req: Request, res: Response) => {
+    try{
+        const apiRick = await axios.get('https://rickandmortyapi.com/api/character')
+        const data = apiRick.data.results;
+        
+        res.json(data);
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({ error: 'Internal Server Error' })
+    }
 })
 
 app.get('/alternative', (req: Request, res: Response) => {
     res.send('This is alternative route')
 })
-
-//Add new route here
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`)
